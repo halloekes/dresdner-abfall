227bff58c46bfc1e8a22081621bf088c062a7864
halloekes <martin.wanitschke@gmail.com>
2026-04-09 21:10:42 +0000

    feat: Adress-Autocomplete und Multi-Adress-Verwaltung
    - Straßen-Autocomplete via Dresden AbfallApp Wicket API
    - Hausnummer-Dropdown nach Straßenauswahl
    - Nutzer sieht keine rohen IDs mehr
    - Mehrere Adressen speicherbar und umschaltbar
    - Rückwärtskompatibel mit altem Datenformat
    - Service Worker Cache-Version erhöht

--- sw.js
+++ sw.js
@@ -1,1 +1,27 @@
+const CACHE_NAME = 'dresdner-abfall-v3';
 
+self.addEventListener('install', event => {
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.map(cacheName => {
+          if (cacheName !== CACHE_NAME) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    }).then(() => self.clients.claim())
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
+    })
+  );
+});
